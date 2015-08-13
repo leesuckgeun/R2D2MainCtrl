fe3af8413e02c66ca75e2e758a6ee2f990b30c94
leesuckgeun <beast0460@gmail.com>
2015-08-13 23:44:45 +0900

    Added Icon that Floating around

--- app/src/main/java/r2d2/lee2345/com/r2d2mainctrl/InitiatingService.java
+++ app/src/main/java/r2d2/lee2345/com/r2d2mainctrl/InitiatingService.java
@@ -2,7 +2,13 @@ package r2d2.lee2345.com.r2d2mainctrl;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 /**
  * This is the face of r2d2. this service displays R2D2's face
@@ -11,6 +17,10 @@ import android.os.IBinder;
  */
 public class InitiatingService extends Service {
 
+    private boolean isViewAttached;
+    private ImageView androidImageView;
+    private WindowManager windowManager;
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -18,10 +28,84 @@ public class InitiatingService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
+        if (!isViewAttached) {
+            windowManager.addView(androidImageView, androidImageView.getLayoutParams());
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        WindowManager.LayoutParams params = initViewToService();
+        initFloatingMotion(params);
+    }
+
+
+    private WindowManager.LayoutParams initViewToService() {
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        androidImageView = new ImageView(this);
+        androidImageView.setImageResource(R.drawable.android);
+        windowManager.addView(androidImageView, params);
+
+        isViewAttached = true;
+
+        return params;
+    }
+
+    private void initFloatingMotion(final WindowManager.LayoutParams params) {
+        androidImageView.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(androidImageView, params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
 
 
 
-        return START_NOT_STICKY;
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        removeView();
+    }
+
+    private void removeView() {
+        if (androidImageView != null) {
+            windowManager.removeView(androidImageView);
+            isViewAttached = false;
+        }
     }
 }
